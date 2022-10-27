71da16fc01ac282effab6629d6fcccc3a73df14c
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-10-27 10:37:55 +0600

    125

--- src/medianoftwosortedarrays/MedianOfTwoSortedArrays.java
+++ src/medianoftwosortedarrays/MedianOfTwoSortedArrays.java
@@ -4,6 +4,8 @@
  */
 package medianoftwosortedarrays;
 
+import javax.print.attribute.standard.MediaSize;
+
 /**
  *
  * @author Reazul-System
@@ -15,6 +17,26 @@ public class MedianOfTwoSortedArrays {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int nums1[] = new int[5];
+        nums1[0] = 4;
+        nums1[1] = 7;
+        nums1[2] = 11;
+        nums1[3] = 15;
+        nums1[4] = 19;
+        nums1[5] = 22;
+        
+        int nums2[] = new int[6];
+        nums2[0] = 3;
+        nums2[1] = 6;
+        nums2[2] = 8;
+        nums2[3] = 10;
+        nums2[4] = 13;
+        nums2[5] = 17;
+        nums2[6] = 20;
+        
+        Solution solution = new Solution();
+        double median = solution.findMedianSortedArrays(nums1, nums2);
+        
+        System.out.println("Median of Two Sorted Array is: " + median);
     }
-    
 }
